ba17a9af13d5114a524546e4a8bb416fc1fe672c
George <grreulvi@gmail.com>
2017-12-08 12:51:04 +0300

    Changed connection settings

--- posh/down.ps1
+++ posh/down.ps1
@@ -3,7 +3,7 @@
 #
 param( [string]$buildVersion )
 
-$versionPattern = "[PR]L_NTG5_[0-9]{3}.[0-9]{2}[0-9a-z]_[0-9A-Z]{8}"
+$versionPattern = "[PR]L_DLC_[0-9]{3}.[0-9]{2}[0-9a-z]_[0-9A-Z]{8}"
 $userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36" 
 
 # getting proxy credentials
@@ -27,24 +27,17 @@ $request = New-Object System.Net.WebClient
 $request.UseDefaultCredentials = $true              # proxy credentials only
 $request.Proxy.Credentials = $request.Credentials
 
-#$url = "http://hifiwscc07.ad.harman.com/cgi-bin/dai-ntg5/dbis.pl"
-#$current = "PL_NTG5_230.408_16216AC2"
-#$current = "PL_NTG5_230.408_16216AC2_M067"
-$url = "http://hifiwscc07.ad.harman.com/cgi-bin/dai-ntg5/dbis.pl?current=$current"
+
+$url = "http://servername.com/cgi-bin/script.pl?current=$current"
 $out = "F:\\docs\\ps_down_file.html"
 $request.Headers.Add( "User-Agent", $userAgent )
 $request.DownloadFile( $url, $out )
 write "Download complete"
 
-$baselinePat = '\<td\>[^<]+?TCFG[^<]+?\<.td\>[trd<>/ ]+?([0-9]+?)\<.td>'
-# Get-Content $out | WHERE { $_ -match "\<tr>[^<]TCFG[^<]\<td\>([0-9]+)\<\/td\>","$1" }
-#$str = "<td><a href='file:'></a></td></tr><tr><td>Date of Latest Submitted TCFG</td> <td>2016/05/27 19:40:42</td></tr><tr><td>//Daimler_NTG_5/Public/TCFG/Development/Default/... up to</td><td>4653046</td></tr><tr><td colspan='2'><h3>PD Applications</h3></td></tr>" 
+$baselinePat = '\<td\>[^<]+?THFD[^<]+?\<.td\>[trd<>/ ]+?([0-9]+?)\<.td>'
+# Get-Content $out | WHERE { $_ -match "\<tr>[^<]THFD[^<]\<td\>([0-9]+)\<\/td\>","$1" }
+
 gc $out | % { if( $_ -match $baselinePat ) { Write-Host  "Baseline: $($matches[1])" -foregroundcolor "magenta" } }
 # if( $str -match $pat ) {
 #     Write-host "code: $($matches[1])"
 # }
-
-#"<td><a href='file:'></a></td></tr><tr><td>Date of Latest Submitted TCFG</td> <td>2016/05/27 19:40:42</td></tr><tr><td>//Daimler_NTG_5/Public/TCFG/Development/Default/... up to</td><td>4653046</td></tr><tr><td colspan='2'><h3>PD Applications</h3></td></tr>" | Select-string '\<tr\>[^<]TCFG[^<]\<td\>([0-9]+)\<\/td\>'
-#"<td><a href='file:'></a></td></tr><tr><td>Date of Latest Submitted TCFG</td> <td>2016/05/27 19:40:42</td></tr><tr><td>//Daimler_NTG_5/Public/TCFG/Development/Default/... up to</td><td>4653046</td></tr><tr><td colspan='2'><h3>PD Applications</h3></td></tr>" | Select-string -pattern 'TCFG'
-
-
