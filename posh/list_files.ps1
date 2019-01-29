e015ccbc3ca9417a6329821fd9e8cf92c3d95342
George <grreulvi@gmail.com>
2019-01-29 15:55:08 +0300

    Cleaned posh directory

--- posh/list_files.ps1
+++
@@ -1,7 +0,0 @@
-# âûáðàòü ôàéëû .hpp è .cpp èç äèðåêòîðèè 
-
-[string]$PATTERN='\.cpp$|\.hpp$'
-[string]$PATH='F:\path\to\files'
-[string]$OUT_FILE='.\filelist.txt'
-
-Get-Childitem -path $PATH -recurse | Where-Object {$_.name -match $PATTERN} | Select-Object FullName | Out-File $OUT_FILE -encoding "UTF8" 
